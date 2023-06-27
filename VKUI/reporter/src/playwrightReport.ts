93e20642a1f6e77da3f5c7d58f20f960902079ea
Evgeniya <e.polozova@corp.vk.com>
2023-06-27 11:41:58 +0300

    feat: add @axe-core/playwright a11y report

--- VKUI/reporter/src/playwrightReport.ts
+++ VKUI/reporter/src/playwrightReport.ts
@@ -12,7 +12,11 @@ function hasFailedScreenshots() {
   return isDataDirExist || isTraceDirExist;
 }
 
-export async function playwrightReport(url: string, token: string) {
+export async function playwrightReport(
+  token: string,
+  playwrightReportUrl: string,
+  axeReportUrl?: string,
+) {
   const gh = github.getOctokit(token);
   const comment = new GitHubCommentBuilder(gh);
 
@@ -22,7 +26,23 @@ export async function playwrightReport(url: string, token: string) {
     message.push('> ⚠️ Some screenshots were failed. See Playwright Report. \n\n');
   }
 
-  message.push(`<a href="${url}" target="_blank">Playwright Report</a>`.replace(/(^|\n) +/g, ''));
+  message.push(
+    `<a href="${playwrightReportUrl}" target="_blank">Playwright Report</a>`.replace(
+      /(^|\n) +/g,
+      '',
+    ),
+  );
+
+  if (axeReportUrl) {
+    message.push('## ♿ a11y tests\n\n');
+
+    message.push(
+      `<a href="${axeReportUrl}" target="_blank">Axe Accessibility Report</a>`.replace(
+        /(^|\n) +/g,
+        '',
+      ),
+    );
+  }
 
   comment.add(message.join(' '));
   await comment.write();
