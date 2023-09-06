63b1d6cc16cd928362fb753b6936d80bf5f2fb2c
Daniil Suvorov <severecloud@gmail.com>
2023-09-06 10:51:36 +0300

    fix(VKUI patch): getBooleanInput fix required Исправленная версия getBooleanInput функции из `@actions/core`
    - https://github.com/actions/toolkit/issues/844

---
+++ VKUI/patch/src/getBooleanInput.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core';
+
+const trueValue = ['true', 'True', 'TRUE'];
+const falseValue = ['false', 'False', 'FALSE'];
+
+/**
+ * Исправленная версия getBooleanInput функции из `@actions/core`.
+ *
+ * [getBooleanInput ignores options.required](https://github.com/actions/toolkit/issues/844)
+ */
+export function getBooleanInput(name: string, options?: core.InputOptions): boolean {
+  const val = core.getInput(name, options);
+
+  if (!val) return false;
+  if (trueValue.includes(val)) return true;
+  if (falseValue.includes(val)) return false;
+
+  throw new TypeError(
+    `Input does not meet YAML 1.2 "Core Schema" specification: ${name}\n` +
+      `Support boolean input list: \`true | True | TRUE | false | False | FALSE\``,
+  );
+}
