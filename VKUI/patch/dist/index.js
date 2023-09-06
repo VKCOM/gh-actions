effe148043e34b412bc2c48276346ec3ae2bcb1b
SevereCloud <SevereCloud@users.noreply.github.com>
2023-09-06 12:34:12 +0000

    build(actions): update dist

--- VKUI/patch/dist/index.js
+++ VKUI/patch/dist/index.js
@@ -14109,6 +14109,60 @@ try {
 
 /***/ }),
 
+/***/ 2048:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getBooleanInput = void 0;
+const core = __importStar(__nccwpck_require__(7117));
+const trueValue = ['true', 'True', 'TRUE'];
+const falseValue = ['false', 'False', 'FALSE'];
+/**
+ * Исправленная версия getBooleanInput функции из `@actions/core`.
+ *
+ * [getBooleanInput ignores options.required](https://github.com/actions/toolkit/issues/844)
+ */
+function getBooleanInput(name, options) {
+    const val = core.getInput(name, options);
+    if (!val)
+        return false;
+    if (trueValue.includes(val))
+        return true;
+    if (falseValue.includes(val))
+        return false;
+    throw new TypeError(`Input does not meet YAML 1.2 "Core Schema" specification: ${name}\n` +
+        `Support boolean input list: \`true | True | TRUE | false | False | FALSE\``);
+}
+exports.getBooleanInput = getBooleanInput;
+
+
+/***/ }),
+
 /***/ 8128:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -14226,6 +14280,7 @@ const semver_1 = __nccwpck_require__(7546);
 const message_1 = __nccwpck_require__(4818);
 const getMergeData_1 = __nccwpck_require__(8128);
 const stableBranchName_1 = __nccwpck_require__(6291);
+const getBooleanInput_1 = __nccwpck_require__(2048);
 function getPrNumber() {
     const pullRequest = github.context.payload.pull_request;
     if (!pullRequest) {
@@ -14260,7 +14315,7 @@ function run() {
             const token = core.getInput('token', { required: true });
             const directory = core.getInput('directory');
             const targetBranchInput = core.getInput('targetBranch');
-            const needScreenshots = core.getBooleanInput('needScreenshots');
+            const needScreenshots = (0, getBooleanInput_1.getBooleanInput)('needScreenshots');
             const pullNumber = getPrNumber();
             const gh = github.getOctokit(token);
             const mergeData = yield (0, getMergeData_1.getMergeData)(gh, github.context.repo, pullNumber);
