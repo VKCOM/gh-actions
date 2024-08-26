9ecb5b878028ea4f4dee3d87fb6d997f9c0594ed
e.muhamethanov <eldarmu.1m@gmail.com>
2024-08-26 11:27:04 +0300

    tech: add floating_ui_version in input

--- VKUI/calculate-updated-floating-ui-version/src/main.ts
+++ VKUI/calculate-updated-floating-ui-version/src/main.ts
@@ -1,22 +1,16 @@
 import * as core from '@actions/core';
 import semver from 'semver';
-import fs from 'fs';
-
-function getPackageVersion(packagePath: string) {
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-  return packageJson.version;
-}
 
 async function run() {
   try {
-    const floatingUiPath = 'packages/vkui-floating-ui/package.json';
     const prevFloatingUiReactDomVersion = core.getInput('prev_version', { required: true });
     const newFloatingUiReactDomVersion = core.getInput('new_version', { required: true });
+    const floatingUIVersion = core.getInput('floating_ui_version', { required: true });
     const updateType = semver.diff(
       prevFloatingUiReactDomVersion,
       newFloatingUiReactDomVersion,
     ) as semver.ReleaseType;
-    const newVKUIFloatingVersion = semver.inc(getPackageVersion(floatingUiPath), updateType);
+    const newVKUIFloatingVersion = semver.inc(floatingUIVersion, updateType);
     core.setOutput('new_version', newVKUIFloatingVersion);
   } catch (e) {}
 }
