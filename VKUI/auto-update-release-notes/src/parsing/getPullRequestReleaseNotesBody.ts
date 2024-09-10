bc1f4d5432ad06f9e94e6afb79b47e11471540b9
EldarMuhamethanov <61377022+EldarMuhamethanov@users.noreply.github.com>
2024-09-10 17:04:49 +0300

    fix: fix pull request body getting (#455)
    Сейчас есть проблема в том, что при парсинге pull request body, если не удалить комментарий после ReleaseNotes, он тоже попадает под парсинг. В результате мы получаем некорректный результат
    
    Добавил обрезание комментария перед парсингом release notes в PR

--- VKUI/auto-update-release-notes/src/parsing/getPullRequestReleaseNotesBody.ts
+++ VKUI/auto-update-release-notes/src/parsing/getPullRequestReleaseNotesBody.ts
@@ -1,10 +1,13 @@
 const RELEASE_NOTE_HEADER = '## Release notes';
+const COMMENT_START = '<!-- ';
 
 export const getPullRequestReleaseNotesBody = (body: string): string | null => {
   const releaseNotesIndex = body.indexOf(RELEASE_NOTE_HEADER);
   if (releaseNotesIndex === -1) {
     return null;
   }
+  const commentStart = body.indexOf(COMMENT_START, releaseNotesIndex);
+  const end = commentStart !== -1 ? commentStart : body.length;
 
-  return body.slice(releaseNotesIndex + RELEASE_NOTE_HEADER.length).trim();
+  return body.slice(releaseNotesIndex + RELEASE_NOTE_HEADER.length, end).trim();
 };
