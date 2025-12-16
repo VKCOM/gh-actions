a3cc7c21c1b73a065e8d7f4875e7871bb6b2061f
SevereCloud <14944123+SevereCloud@users.noreply.github.com>
2025-12-16 08:18:04 +0000

    build(actions): update dist

--- VKUI/reporter/dist/index.js
+++ VKUI/reporter/dist/index.js
@@ -18293,9 +18293,9 @@ var require_io = __commonJS({
   }
 });
 
-// ../../node_modules/@actions/exec/lib/toolrunner.js
+// ../../node_modules/@actions/core/node_modules/@actions/exec/lib/toolrunner.js
 var require_toolrunner = __commonJS({
-  "../../node_modules/@actions/exec/lib/toolrunner.js"(exports2) {
+  "../../node_modules/@actions/core/node_modules/@actions/exec/lib/toolrunner.js"(exports2) {
     "use strict";
     var __createBinding = exports2 && exports2.__createBinding || (Object.create ? (function(o, m, k, k2) {
       if (k2 === void 0) k2 = k;
@@ -18777,9 +18777,9 @@ var require_toolrunner = __commonJS({
   }
 });
 
-// ../../node_modules/@actions/exec/lib/exec.js
+// ../../node_modules/@actions/core/node_modules/@actions/exec/lib/exec.js
 var require_exec = __commonJS({
-  "../../node_modules/@actions/exec/lib/exec.js"(exports2) {
+  "../../node_modules/@actions/core/node_modules/@actions/exec/lib/exec.js"(exports2) {
     "use strict";
     var __createBinding = exports2 && exports2.__createBinding || (Object.create ? (function(o, m, k, k2) {
       if (k2 === void 0) k2 = k;
--- VKUI/s3/dist/index.js
+++ VKUI/s3/dist/index.js
@@ -18301,9 +18301,9 @@ var require_io = __commonJS({
   }
 });
 
-// ../../node_modules/@actions/exec/lib/toolrunner.js
+// ../../node_modules/@actions/core/node_modules/@actions/exec/lib/toolrunner.js
 var require_toolrunner = __commonJS({
-  "../../node_modules/@actions/exec/lib/toolrunner.js"(exports2) {
+  "../../node_modules/@actions/core/node_modules/@actions/exec/lib/toolrunner.js"(exports2) {
     "use strict";
     var __createBinding2 = exports2 && exports2.__createBinding || (Object.create ? (function(o, m, k, k2) {
       if (k2 === void 0) k2 = k;
@@ -18785,9 +18785,9 @@ var require_toolrunner = __commonJS({
   }
 });
 
-// ../../node_modules/@actions/exec/lib/exec.js
+// ../../node_modules/@actions/core/node_modules/@actions/exec/lib/exec.js
 var require_exec = __commonJS({
-  "../../node_modules/@actions/exec/lib/exec.js"(exports2) {
+  "../../node_modules/@actions/core/node_modules/@actions/exec/lib/exec.js"(exports2) {
     "use strict";
     var __createBinding2 = exports2 && exports2.__createBinding || (Object.create ? (function(o, m, k, k2) {
       if (k2 === void 0) k2 = k;
