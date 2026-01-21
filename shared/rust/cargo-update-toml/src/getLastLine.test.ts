f65257e72a99345d9832cd002d3319e6ee2240b6
Daniil Suvorov <severecloud@gmail.com>
2026-01-21 11:19:05 +0300

    fix: add banner for require

--- shared/rust/cargo-update-toml/src/getLastLine.test.ts
+++ shared/rust/cargo-update-toml/src/getLastLine.test.ts
@@ -1,16 +1,16 @@
-import * as assert from "node:assert/strict";
-import { Readable } from "node:stream";
-import { describe, it } from "node:test";
-import { getLastLine } from "./getLastLine.ts";
+import * as assert from 'node:assert/strict';
+import { Readable } from 'node:stream';
+import { describe, it } from 'node:test';
+import { getLastLine } from './getLastLine.ts';
 
-describe("getLastLine", () => {
-  it("returns the last line from a stream", async () => {
+describe('getLastLine', () => {
+  it('returns the last line from a stream', async () => {
     const stream = new Readable();
 
-    stream.push([1, 2, 3, 4].join("\n"));
+    stream.push([1, 2, 3, 4].join('\n'));
     stream.push(null);
 
     const result = await getLastLine(stream);
-    assert.strictEqual(result, "4");
+    assert.strictEqual(result, '4');
   });
 });
--- shared/rust/cargo-update-toml/src/registry.test.ts
+++ shared/rust/cargo-update-toml/src/registry.test.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-floating-promises -- node тесты*/
 
-import * as assert from "node:assert/strict";
-import { describe, it } from "node:test";
-import { cargoRegistryLastIndexPackage } from "./registry.ts";
+import * as assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+import { cargoRegistryLastIndexPackage } from './registry.ts';
 
-describe("cargoRegistryLastIndexPackage", () => {
-  it("get md4", async () => {
-    const lastPackage = await cargoRegistryLastIndexPackage("md4");
+describe('cargoRegistryLastIndexPackage', () => {
+  it('get md4', async () => {
+    const lastPackage = await cargoRegistryLastIndexPackage('md4');
 
-    assert.strictEqual(lastPackage.name, "md4");
+    assert.strictEqual(lastPackage.name, 'md4');
   });
 });
